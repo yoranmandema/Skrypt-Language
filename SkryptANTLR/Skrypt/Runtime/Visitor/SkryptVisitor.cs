ed4318011a75bed92d9eae23ed8584ef69a0abbd
Blieper <yoranmandema@eml.cc>
2019-07-17 00:48:28 +0200

    use self for left hand side in custom operators

--- SkryptANTLR/Skrypt/Runtime/Visitor/SkryptVisitor.cs
+++ SkryptANTLR/Skrypt/Runtime/Visitor/SkryptVisitor.cs
@@ -509,7 +509,7 @@ namespace Skrypt {
             switch (operationName) {
                 case "+":
                     if (left.AsType<BaseInstance>().TypeObject.Traits.OfType<AddableTrait>().Any()) {
-                        result = left.AsType<BaseInstance>().GetProperty("Add").Value.AsType<FunctionInstance>().Run(left, right);
+                        result = left.AsType<BaseInstance>().GetProperty("Add").Value.AsType<FunctionInstance>().RunOnSelf(left, right);
                     } else { 
                         result = _engine.ExpressionInterpreter.EvaluatePlusExpression(left, right);
                     }
