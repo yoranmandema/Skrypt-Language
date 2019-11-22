a42e44865f5387835443ee73a07fc7ffdc4a90ef
Blieper <yoranmandema@eml.cc>
2019-11-22 23:29:41 +0100

    Add unit tests

---
+++ SkryptANTLR/Skrypt.Tests/ExpressionTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Skrypt;
+
+namespace Skrypt.Tests {
+    public class ExpressionTests {
+        [Theory]
+        [InlineData("1 + 1", 2)]
+        [InlineData("2 * 4", 8)]
+        [InlineData("2 * 4 + 2", 10)]
+        [InlineData("2 + 2 * 4", 10)]
+        [InlineData("15 / 3", 5)]
+        [InlineData("15 / 3 + 2", 7)]
+        [InlineData("2 + 15 / 3", 7)]
+        [InlineData("(2 + 13) * 2", 30)]
+        public void ShouldEvaluateNumericExpressions(string source, double expected) {
+            var value = new Engine().Run(source).CompletionValue;
+
+            Assert.NotNull(value);
+            Assert.Equal(expected, value.AsType<NumberInstance>().Value);
+        }
+
+        [Theory]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("true && true", true)]
+        [InlineData("true && false", false)]
+        [InlineData("false && true", false)]
+        [InlineData("false && false", false)]
+        [InlineData("true || true", true)]
+        [InlineData("true || false", true)]
+        [InlineData("false || true", true)]
+        [InlineData("false || false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        public void ShouldEvaluateBooleanExpressions(string source, bool expected) {
+            var value = new Engine().Run(source).CompletionValue;
+
+            Assert.NotNull(value);
+            Assert.Equal(expected, value.AsType<BooleanInstance>().Value);
+        }
+    }
+}
---
+++ SkryptANTLR/Skrypt.Tests/LiteralTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+using Skrypt;
+
+namespace Skrypt.Tests {
+    public class LiteralTests {
+        [Fact]
+        public void ShouldParseNull() {
+            var nullValue = new Engine().Run("null").CompletionValue;
+
+            Assert.Null(nullValue);
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(42, "42")]
+        [InlineData(0.14, "0.14")]
+        [InlineData(3.14159, "3.14159")]
+        public void ShouldParseNumericLiterals(object expected, string source) {
+            var value = new Engine().Run(source).CompletionValue;
+
+            Assert.NotNull(value);
+            Assert.Equal(Convert.ToDouble(expected), value.AsType<NumberInstance>().Value);
+        }
+
+        [Theory]
+        [InlineData("\"test\"", "test")]
+        [InlineData("\"\"", "")]
+        [InlineData("\"\\n\"", "\n")]
+        public void ShouldParseStringLiterals(string source, string expected) {
+            var value = new Engine().Run(source).CompletionValue;
+
+            Assert.NotNull(value);
+            Assert.Equal(expected, value.AsType<StringInstance>().Value);
+        }
+
+        [Theory]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        public void ShouldParseBoolLiterals(string source, bool expected) {
+            var value = new Engine().Run(source).CompletionValue;
+
+            Assert.NotNull(value);
+            Assert.Equal(expected, value.AsType<BooleanInstance>().Value);
+        }
+    }
+}
