a9a9c9205b57650bcac78dbe1ebf8e28cca01111
Blieper <yoranmandema@eml.cc>
2019-11-25 17:11:00 +0100

    throw exception if no valid signature was found

--- SkryptLanguage/Skrypt/CLR/CLRMethodGroup.cs
+++ SkryptLanguage/Skrypt/CLR/CLRMethodGroup.cs
@@ -8,19 +8,32 @@ using System.Reflection;
 namespace Skrypt.CLR {
     public class CLRMethodGroup : IFunction {
         public string Name { get; set; }
-        public CLRFunction[] functions;
+        public CLRMethod[] Functions { get; set; }
 
         public BaseObject Run(Engine engine, BaseObject self, Arguments arguments) {
             BaseObject result = null;
+            bool validArguments = false;
 
-            foreach (var function in functions) {
+            foreach (var function in Functions) {
                 var args = function.ConvertArguments(arguments, out bool isValid);
 
-                if (!isValid) continue;
+                if (isValid) {
+                    validArguments = isValid;
 
-                var res = function.del.DynamicInvoke(args);
+                    var res = function.del.DynamicInvoke(args);
 
-                result = CLRTypeConverter.ConvertToSkryptObject(engine, res);
+                    result = CLRTypeConverter.ConvertToSkryptObject(engine, res);
+
+                    break;
+                }
+            }
+
+            if (!validArguments) {
+                var argString = "";
+
+                foreach (var arg in arguments.Values) argString += arg.GetType().Name + " ";
+
+                throw new ArgumentException($"No method found for arguments {argString}");
             }
 
             return result;
