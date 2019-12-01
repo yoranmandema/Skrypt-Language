c1f778a3761181f2812616876c707e99930df606
Blieper <yoranmandema@eml.cc>
2019-12-01 22:19:10 +0100

    Optimise VisitAssignNameStatement

--- SkryptLanguage/Skrypt/Runtime/Visitor/SkryptVisitor.VisitAssignNameStatement.cs
+++ SkryptLanguage/Skrypt/Runtime/Visitor/SkryptVisitor.VisitAssignNameStatement.cs
@@ -8,15 +8,14 @@ namespace Skrypt {
         public override SkryptObject VisitAssignNameStatement(SkryptParser.AssignNameStatementContext context) {
             var variable = CurrentEnvironment.GetVariable(context.name().GetText());
 
-            if (variable.IsConstant) {
+            if (variable.IsConstant)
                 _engine.ErrorHandler.FatalError(context.Start, "Constant cannot be redefined.");
-            }
 
             var value = Visit(context.expression());
 
             if (value is IValue noref) value = noref.Copy();
 
-            var op = (context.assign() as SkryptParser.AssignOperatorContext).Operator;
+            var op = ((SkryptParser.AssignOperatorContext)context.assign()).Operator;
 
             if (op != null) value = EvaluateExpression(op.Text, variable.Value, value, op);
 
