f58145750e5bc8e3c82590ea60af5524e780c006
Blieper <yoranmandema@eml.cc>
2019-12-01 22:37:18 +0100

    Cleanup VisitBooleanLiteral

--- SkryptLanguage/Skrypt/Runtime/Visitor/Literals/SkryptVisitor.VisitBooleanLiteral.cs
+++ SkryptLanguage/Skrypt/Runtime/Visitor/Literals/SkryptVisitor.VisitBooleanLiteral.cs
@@ -6,12 +6,7 @@ using Skrypt.ANTLR;
 namespace Skrypt {
     internal partial class SkryptVisitor : SkryptBaseVisitor<SkryptObject> {
         public override SkryptObject VisitBooleanLiteral(SkryptParser.BooleanLiteralContext context) {
-            var value = context.boolean().value;
-            var boolean = _engine.CreateBoolean(value);
-
-            LastResult = boolean;
-
-            return boolean;
+            return LastResult = _engine.CreateBoolean(context.boolean().value);
         }
     }
 }
