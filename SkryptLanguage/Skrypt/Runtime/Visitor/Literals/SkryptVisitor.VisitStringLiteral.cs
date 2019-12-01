f896a32dd56b4801670cdc7637e81f880e1cb812
Blieper <yoranmandema@eml.cc>
2019-12-01 22:38:36 +0100

    Cleanup VisitStringLiteral

--- SkryptLanguage/Skrypt/Runtime/Visitor/Literals/SkryptVisitor.VisitStringLiteral.cs
+++ SkryptLanguage/Skrypt/Runtime/Visitor/Literals/SkryptVisitor.VisitStringLiteral.cs
@@ -6,12 +6,7 @@ using Skrypt.ANTLR;
 namespace Skrypt {
     internal partial class SkryptVisitor : SkryptBaseVisitor<SkryptObject> {
         public override SkryptObject VisitStringLiteral(SkryptParser.StringLiteralContext context) {
-            var value = context.@string().value;
-            var str = _engine.CreateString(value);
-
-            LastResult = str;
-
-            return str;
+            return _engine.CreateString(context.@string().value);
         }
     }
 }
